3a7e3e5c385321839851b12f9b43e1365c16c07f
cyber-lama <dimmeln1k@yandex.ru>
2021-08-11 20:14:26 +0300

    - Построение базы данных, миграции: UserData.php

---
+++ database/migrations/2021_08_11_165448_create_user_data_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('email', 100)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->tinyInteger('phone')->unique();
+            $table->tinyInteger('gender')->nullable();
+            $table->timestamp('birthday')->nullable();
+            $table->text('region');
+            $table->text('city');
+            $table->text('street');
+            $table->text('house');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_data');
+    }
+}
