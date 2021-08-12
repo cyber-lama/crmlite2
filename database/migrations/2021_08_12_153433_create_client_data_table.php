13509182cfaaeaa8e4aeba16da82dea7e648f549
cyber-lama <dimmeln1k@yandex.ru>
2021-08-12 18:47:12 +0300

    - Построение базы данных, миграции

---
+++ database/migrations/2021_08_12_153433_create_client_data_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_data', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('phone')->unique();
+            $table->text('region')->nullable();
+            $table->text('city')->nullable();
+            $table->text('street')->nullable();
+            $table->text('house')->nullable();
+            $table->foreignId('client_id')->constrained('clients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_data');
+    }
+}
