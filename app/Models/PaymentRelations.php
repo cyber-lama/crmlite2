864bff4fae1a733bc77b8229b380b34e099e5c9a
cyber-lama <dimmeln1k@yandex.ru>
2021-08-12 19:26:55 +0300

    - Построение базы данных, миграции

---
+++ app/Models/PaymentRelations.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentRelations extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_08_12_161604_create_task_relations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskRelationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_relations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('task_id')->constrained('tasks')->onDelete('cascade');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->bigInteger('client_id')->unsigned();
+            $table->foreign('client_id')->references('id')->on('clients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_relations');
+    }
+}
